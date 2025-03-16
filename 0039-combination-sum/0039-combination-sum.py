3c05f9b271888838887f4ca13e4fe31458e869a0
Vishnu Paturu <42509698+vistej@users.noreply.github.com>
2025-03-15 22:18:26 -0400

    Time: 7 ms (82.98%), Space: 17.9 MB (61.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,21 +2,16 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
 
         res = []
-        checked = set()
 
-        def backtrack(ca, s):
-            for c in candidates:
-                if s + c == target:
-                    na = ca + [c]
-                    na.sort()
-                    st = ''.join(map(str, na))
-                    if st not in checked:
-                        res.append(na)
-                        checked.add(st)
-                elif s + c < target:
-                    backtrack(ca + [c], s + c)
+        def backtrack(i, ca, s):
+            for j in range(i, len(candidates)):
+                if s + candidates[j] == target:
+                    na = ca + [candidates[j]]
+                    res.append(na)
+                elif s + candidates[j] < target:
+                    backtrack(j, ca + [candidates[j]], s + candidates[j] )
         
-        backtrack([], 0)
+        backtrack(0, [], 0)
 
         return res
         
