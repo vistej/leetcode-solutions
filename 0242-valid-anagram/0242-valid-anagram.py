5a1ac502a5e47cad263dc4bdb89918b7e3cf8673
Vishnu Paturu <42509698+vistej@users.noreply.github.com>
2025-07-09 16:24:21 -0400

    Time: 15 ms (41.98%), Space: 18 MB (32.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,15 +3,15 @@ class Solution:
         if len(s) != len(t):
             return False
         obj = {}
-
         for i in range(len(s)):
             obj[s[i]] = obj.get(s[i], 0) + 1
         
-        for i in range(len(s)):
+        for i in range(len(t)):
             if t[i] not in obj:
                 return False
             obj[t[i]] -= 1
             if not obj[t[i]]:
                 del obj[t[i]]
+            
         
         return len(obj) == 0
