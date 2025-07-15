702cae6ba815b6b0f8ce7631e8a81b7541a160f7
Vishnu Paturu <42509698+vistej@users.noreply.github.com>
2025-07-15 19:25:42 -0400

    Time: 15 ms (86.32%), Space: 80.6 MB (35.28%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -6,12 +6,9 @@ var dailyTemperatures = function(temperatures) {
     const res = Array(temperatures.length).fill(0);
     let stack = [temperatures.length - 1];
     for (let i = temperatures.length - 2; i >= 0; i--) {
-        let days = 0;
         while (stack.length) {
             const last = stack.pop();
-            if (temperatures[i] >= temperatures[last]) {
-                days++;
-            } else {
+            if (temperatures[i] < temperatures[last]) {
                 stack.push(last)
                 res[i] = last - i;
                 break;
