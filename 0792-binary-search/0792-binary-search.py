1ea197270ee6549304d98726a32b8386d41cfd1f
Vishnu Paturu <42509698+vistej@users.noreply.github.com>
2025-07-16 14:24:13 -0400

    Time: 0 ms (100%), Space: 25.9 MB (6.13%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,19 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if nums[0] > target or nums[-1] < target:
-            return -1
-        
         def bin(i, j):
-            m = (i + j) // 2
-
-            if nums[m] == target:
-                return m
-            if i == j:
+            mid = (i + j) // 2
+            if nums[mid] == target:
+                return mid
+            if mid == i or mid == j:
                 return -1
-            if nums[m] > target:
-                return bin(i, m - 1)
+            if nums[mid] < target:
+                return bin(mid, j)
             else:
-                return bin(m + 1, j)
-
-        
-        return bin(0, len(nums) - 1)
+                return bin(i, mid)
+        return bin(0, len(nums))
