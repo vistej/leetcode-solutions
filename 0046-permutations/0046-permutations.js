7f6e31ef4908904393b9bd08d59f38f76105513a
Vishnu Paturu <42509698+vistej@users.noreply.github.com>
2025-07-27 18:04:32 -0400

    Time: 3 ms (45.7%), Space: 59.8 MB (49.96%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -3,29 +3,15 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-
     const res = [];
-
-    const swap = (i, j) => {
-        const temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    const backtrack = (i) => {
-        if (i === nums.length) {
-            res.push([...nums]);
-            return;
-        }
-
+    function loop(i) {
+        if (i === nums.length) res.push([...nums]);
         for (let j = i; j < nums.length; j++) {
-            swap(i, j);
-            backtrack(i + 1);
-            swap(i, j);
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            loop(i + 1);
+            [nums[i], nums[j]] = [nums[j], nums[i]];
         }
     }
-
-    backtrack(0);
+    loop(0);
     return res;
-    
 };
